fca47efa73c9a6cbe631d41c3bea6701843c8021
I0x0I <root@localhost.localdomain>
2024-03-03 20:11:31 -0600

    Updated Patch

--- rootfs/utils/build-ORB_SLAM3.sh
+++ rootfs/utils/build-ORB_SLAM3.sh
@@ -8,21 +8,12 @@ make -C build install -j
 cd ../
 git clone https://github.com/UZ-SLAMLab/ORB_SLAM3.git ORB_SLAM3
 cd ORB_SLAM3
-git apply ~/rootfs/utils/ViewerAR.patch
-sed -i -e 's/c++11/c++14/g' CMakeLists.txt
-sed -i -e 's/OpenCV 4.4/OpenCV/g' CMakeLists.txt
+git apply ~/rootfs/utils/ORB_SLAM3.patch
 ./build.sh
 source /opt/ros/noetic/setup.bash
 rosdep init && rosdep update
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:`pwd`/Examples_old/ROS/
 cd Examples_old/ROS/ORB_SLAM3/
-sed -i -e 's/c++11/c++14/g' CMakeLists.txt
-sed -i -e 's/OpenCV 3.0/OpenCV/g' CMakeLists.txt
-sed -i '49i ${PROJECT_SOURCE_DIR}/../../../Thirdparty/Sophus' CMakeLists.txt
-sed -i '31i #include "../../../include/Converter.h"' src/AR/ros_mono_ar.cc
-#sed -i '152d' src/AR/ros_mono_ar.cc
-sed -i '152s/mpSLAM/ORB_SLAM3::Converter::toCvMat(mpSLAM/g' src/AR/ros_mono_ar.cc
-sed -i '152s/);/).matrix());/g' src/AR/ros_mono_ar.cc
 cmake -B build
 make -C build -j
 
---
+++ rootfs/utils/make-docker-ws.sh
@@ -0,0 +1,12 @@
+set -e
+echo "Creating Folder "$1
+mkdir -p $1/rootfs/src
+touch $1/rootfs/src/.gitkeep
+mkdir -p $1/rootfs/data
+touch $1/rootfs/data/.gitkeep
+echo  "!.gitkeep" > $1/.gitignore
+cd $1
+folder_name=${PWD##*/}
+wget https://cdn.jsdelivr.net/gh/I0x0I/ORB_SLAM3-ws@main/Makefile
+sed -i -e "s/ORB_SLAM3-ws/$folder_name/g" Makefile
+echo "Done. \`cd" `pwd`"\`"
